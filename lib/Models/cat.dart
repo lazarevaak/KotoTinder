f018f3b488124fe6583490f7144c936aa6462585
lazarevaak (Лазарева Александра) <lazarevaak@yandex.ru>
2025-11-18 14:38:06 +0300

    Модель Котика

--- lib/Models/cat.dart
+++ lib/Models/cat.dart
@@ -1,1 +1,32 @@
-class Cat { }
+class Cat {
+    final String id;
+    final String url;
+    final int width;
+    final int height;
+
+    final String? breedID;
+    final String? breedName;
+
+    Cat({
+        required this.id,
+        required this.url,
+        required this.width,
+        required this.height,
+
+        this.breedID,
+        this.breedName,
+    });
+
+    factory Cat.fromJson(Map<String, dynamic> json) {
+        final breeds = json['breeds'] as List<dynamic>?;
+
+        return Cat(
+            id: json['id'], 
+            url: json['url'], 
+            width: json['width'] ?? 0, 
+            height: json['height'] ?? 0,
+            breedID: breeds != null && breeds.isNotEmpty ? breeds[0]['id'] : null,
+            breedName: breeds != null && breeds.isNotEmpty ? breeds[0]['name'] : null
+        );
+    }
+ }
