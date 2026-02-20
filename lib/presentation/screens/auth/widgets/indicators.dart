5f0e26fa3e3fba55b9764100db6f2e855262b95d
lazarevaak (Лазарева Александра) <lazarevaak@yandex.ru>
2026-02-20 23:19:16 +0300

    fix UI 1

---
+++ lib/presentation/screens/auth/widgets/indicators.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class Indicators extends StatelessWidget {
+  const Indicators({
+    required this.length,
+    required this.currentPage,
+  });
+
+  final int length;
+  final int currentPage;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: List.generate(length, (index) {
+        final active = index == currentPage;
+        return AnimatedContainer(
+          duration: const Duration(milliseconds: 300),
+          margin: const EdgeInsets.all(6),
+          height: 8,
+          width: active ? 24 : 8,
+          decoration: BoxDecoration(
+            color:
+                active ? Colors.white : Colors.white38,
+            borderRadius: BorderRadius.circular(20),
+          ),
+        );
+      }),
+    );
+  }
+}
+
